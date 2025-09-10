2c91d48d459e1a1fcf51888ace9d5a8fa2a81123
Gerrit Balindt <gerrit.balindt@gruenecho.de>
2025-09-10 14:36:09 +0200

    add plot information #64

--- components/records/GridView.vue
+++ components/records/GridView.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { onMounted, ref } from 'vue';
     import GridViewTableTab from './GridViewTableTab.vue';
+    import GridViewGridTab from './GridViewGridTab.vue';
 
     const tab = ref(null);
 
@@ -16,7 +17,10 @@
     });
 
     const tabObjects = ref({
-        "plot": {}
+        "plot": {
+            title: "Eckendaten",
+            showAsTable: false
+        }
     });
 
     function findInType(types, key) {
@@ -38,14 +42,35 @@
             const property = schema.properties[key];
             if (findInType(property.type, 'array')) {
                 objects[key] = property;
+                objects[key].showAsTable = true;
             }
         }
+        
         return objects;
+    }
+    function getNonObjectProperties(schema) {
+        const nonObjects = {
+            title: schema.title || 'Details',
+            showAsTable: false
+        };
+        for (const key in schema.properties) {
+            const property = schema.properties[key];
+            if (!findInType(property.type, 'array')) {
+                nonObjects[key] = property;
+                nonObjects[key].showAsTable = false;
+            }
+        }
+        return nonObjects;
     }
 
     onMounted(() => {
-        tabObjects.value = getAllObjectsFromSchema(props.schema);
+        // Get non-object properties
+        tabObjects.value.plot = getNonObjectProperties(props.schema);
+        tabObjects.value = {...tabObjects.value, ...getAllObjectsFromSchema(props.schema)};
         console.log('Tab objects:', tabObjects.value);
+        // initialize tab to first key
+        const firstKey = Object.keys(tabObjects.value)[0];
+        tab.value = firstKey;
     });
 </script>
 
@@ -58,7 +83,9 @@
         <v-tabs-window v-model="tab">
             <!-- not index but key of object-->
             <v-tabs-window-item v-for="(object, key) in tabObjects" :key="key" :value="key">
-                <GridViewTableTab :data="props.data[key]" :schema="object.items" />
+
+                <GridViewTableTab v-if="object.showAsTable" :data="props.data[key]" :schema="object.items" />
+                <GridViewGridTab v-else :data="props.data" :schema="props.schema" />
             </v-tabs-window-item>
         </v-tabs-window>
     </div>
